b9c307c6b27e91feb35f051bc7405327d154eb36
sanju <sanju@pop-os.localdomain>
2022-12-06 19:03:54 +0530

    added user based models

--- StudyBud/urls.py
+++ StudyBud/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 # a list of different urls
@@ -25,3 +27,6 @@ urlpatterns = [
     path("", include('base.urls')),
     path('api/', include('base.api.urls'))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
