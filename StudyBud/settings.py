c527469039e527cfeb5b34d39d73648cef7ac709
sanju <sanju@pop-os.localdomain>
2022-12-06 20:35:39 +0530

    final push

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-e0vioa%2mxw@3t)bbi3*y!tgx15ug^#+yk9!eonkkgm&t6(8bx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,6 +130,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_URL = '/images/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
